abb8316bd6fdf1d0bdb5e7b092b468e99953e6ce
Miguel Barconte <timoteoalexblade@gmail.com>
2023-10-02 21:01:48 -0300

    necesito q suba el target xd

--- api-rest-ejemplo-curso/src/main/java/com/spring/boot/api/models/dao/IUsuarioDao.java
+++
@@ -1,7 +0,0 @@
-package com.spring.boot.api.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.spring.boot.api.entity.Usuario;
-
-public interface IUsuarioDao extends CrudRepository<Usuario, Integer>{}
-
